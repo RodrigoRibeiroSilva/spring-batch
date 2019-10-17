7801283f60204a07f01db11d3b859e73b357304e
RodrigoRibeiroSilva <rodrigo94.ribeiro.silva@gmail.com>
2019-10-16 22:16:48 -0300

    Iniciando o projeto piloto com spring-batch

---
+++ complete/src/main/java/com/example/batchprocessing/BatchConfiguration.java
@@ -0,0 +1,79 @@
+package com.example.batchprocessing;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.FlatFileItemWriter;
+import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.batch.item.file.builder.FlatFileItemWriterBuilder;
+import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
+import org.springframework.batch.item.file.transform.PassThroughLineAggregator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.FileSystemResource;
+
+@Configuration
+@EnableBatchProcessing
+public class BatchConfiguration {
+
+    @Autowired
+    public JobBuilderFactory jobBuilderFactory;
+
+    @Autowired
+    public StepBuilderFactory stepBuilderFactory;
+    
+    @Bean
+    public FlatFileItemReader<Person> reader() {
+        return new FlatFileItemReaderBuilder<Person>()
+            .name("personItemReader")
+            .resource(new ClassPathResource("sample-data.csv"))
+            .delimited()
+            .names(new String[]{"firstName", "lastName"})
+            .fieldSetMapper(new BeanWrapperFieldSetMapper<Person>() {{
+                setTargetType(Person.class);
+            }})
+            .build();
+    }
+
+    @Bean
+    public PersonItemProcessor processor() {
+        return new PersonItemProcessor();
+    }
+    
+    @Bean
+    public FlatFileItemWriter<String> writer() {
+      return new FlatFileItemWriterBuilder<String>()
+          .name("greetingItemWriter")
+          .resource(new FileSystemResource(
+              "target/test-outputs/greetings.txt"))
+          .lineAggregator(new PassThroughLineAggregator<>()).build();
+    }
+
+
+    @Bean
+    public Job importUserJob(JobCompletionNotificationListener listener, Step step1) {
+        return jobBuilderFactory.get("importUserJob")
+            .incrementer(new RunIdIncrementer())
+            .listener(listener)
+            .flow(step1)
+            .end()
+            .build();
+    }
+
+    @Bean
+    public Step step1(FlatFileItemWriter<String> writer) {
+        return stepBuilderFactory.get("step1")
+            .<Person, String> chunk(10)
+            .reader(reader())
+            .processor(processor())
+            .writer(writer)
+            .build();
+    }
+
+}
